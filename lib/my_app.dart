4d966e3676cdaf98a5b81699e37e19103756c246
Sumi2212 <fhb232592@fh-vie.ac.at>
2025-12-30 22:13:11 +0100

    finalisiert

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -5,9 +5,9 @@ class MyApp extends StatelessWidget {
   MyApp({super.key});
 
   final List<Color> colors = [
-    const Color.fromRGBO(232,209,224, 1),
+    const Color.fromARGB(255, 187, 167, 180),
     const Color.fromRGBO(220,220,235, 1),
-    const Color.fromRGBO(216,231,245, 1),
+    const Color.fromARGB(255, 183, 203, 221),
   ];
 
   @override
